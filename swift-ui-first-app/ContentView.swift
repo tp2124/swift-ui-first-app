bebc96e922fc3bd3b74038865034982a9e4ca41e
tprimm <tp2124@gmail.com>
2020-04-24 10:30:34 -0500

    Initial Commit

---
+++ swift-ui-first-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-ui-first-app
+//
+//  Created by Jialin Wang on 4/24/20.
+//  Copyright © 2020 tprimm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
