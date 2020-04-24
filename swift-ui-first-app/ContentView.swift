a2373b3d8dd9f5c6645dd61e8e40768f6128659f
tprimm <tp2124@gmail.com>
2020-04-24 11:40:12 -0500

    List of Content format with multiple items.

--- swift-ui-first-app/ContentView.swift
+++ swift-ui-first-app/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
+        List(0 ..< 5) {item in
             Image(systemName: "photo")
             VStack(alignment: .leading) {
                 Text("Rooms")
