990529fd2b8bdc63a84456f442488713c1cdb9fc
Travis Primm <tp2124@gmail.com>
2020-04-24 12:19:35 -0500

    Adding image

--- docs/notes.md
+++ docs/notes.md
@@ -1,2 +1,3 @@
 # General Xcode
 * Use `cmd + click` in the Canvas, followed by selecting `Inspect` to be able to bring up properties of what's selected in the Canvas. Good for discovery.
+* To add an image to the catalog, you have to open the `Assets.xcassets` in Xcode, and then click and drag your image into that pane to have it be able to be referenced in the project.
---
+++ swift-ui-first-app/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  swift-ui-first-app
+//
+//  Created by Jialin Wang on 4/24/20.
+//  Copyright © 2020 tprimm. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
