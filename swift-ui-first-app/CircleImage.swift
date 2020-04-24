f4f8c02df004bf0b2fbfec4d9e9c994459503eee
Travis Primm <tp2124@gmail.com>
2020-04-24 12:21:08 -0500

    Image with circle outline.

--- swift-ui-first-app/CircleImage.swift
+++ swift-ui-first-app/CircleImage.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
     }
 }
 
